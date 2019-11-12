51f63a70e2dbfdd58f945d7218c9fb06b6626cd4
s-mcdonaldann <s-mcdonaldann@53S50030248.it.bsd405.org>
2019-11-12 11:49:01 -0800

    Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -1,32 +1,57 @@
 /**
  * @author Mr. Rasmussen
  */
-
 package fracCalc;
+import java.util.*;
 
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
+    public static void main(String[] args){
+		Scanner userInput = new Scanner (System.in);
+		System.out.print("Input your fraction: ");
+		String fraction = userInput.nextLine();
+		String firstFrac = "";
+    	while (!fraction.equals("quit")) {
+    		firstFrac = produceAnswer(fraction);
+        	System.out.println(firstFrac);
+    		System.out.print("Input your fraction: ");
+    		fraction = userInput.nextLine();
+    	}
     }
-
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    
+    public static String produceAnswer(String input){
+    	String firstFrac = "";
+    	String operator = "";
+    	String secondFrac = "";
+    	int i = 0;
+    	while (firstFrac == "") {
+    		if (input.charAt(i)== (' ')) {
+    			firstFrac = input.substring(0, i);
+    			operator = input.substring(i+1, i+2);
+    			secondFrac = input.substring(i+3, input.length());
+    		}
+    		else {
+    			i++;
+    		}
+    	}
+		return secondFrac;
     }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-
+//    public static String seperateFrac (String frac) {
+//    	String numerator = "";
+//    	String denominator = "";
+//    	String whole = "";
+//    	int i = 0;
+//    	while (numerator =="") {
+//    		if (frac.charAt(i)== ('_')) {
+//    			whole = frac.substring(0, i);
+//    		}
+//    		else {
+//    			if (i < frac.length()) {
+//    				i++;
+//    			}
+//    			else {
+//    				whole
+//    		}
+//    	}
+//    }
 }
